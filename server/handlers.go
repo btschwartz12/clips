5c146ff1cb6c4f754420e5a8cae9e6d09e47e878
Ben Schwartz <scben@umich.edu>
2025-01-13 23:51:55 -0500

    testing deploy

--- server/handlers.go
+++ server/handlers.go
@@ -19,7 +19,7 @@ type HomePageData struct {
 }
 
 func (s *Server) health(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK!"))
+	w.Write([]byte("OKr"))
 }
 
 func (s *Server) home(w http.ResponseWriter, r *http.Request) {
